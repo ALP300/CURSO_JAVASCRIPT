dcef6b67455b2b54f57b489e92655ebab8d12d05
ALP300 <aitorpalomino1@gmail.com>
2024-09-15 21:01:01 -0500

    Teoria variables y operadores

---
+++ Teoria.js
@@ -0,0 +1,26 @@
+/*VARIABLES
+let num1=4;
+let mensaje="HOLA";
+let num2= 5.4;
+let a= parseFloat(prompt("Ingresa el número entero(a): "));
+let b= parseFloat(prompt("Ingresa el número decimal: "));*/
+
+/*OPERADORES ARITMÉTICOS*/
+let num1= parseFloat(prompt("Ingresa el número 1: "));
+let num2= parseFloat(prompt("Ingresa el número 2: "));
+let suma= num1+num2;
+let resta= num1-num2;
+let mult= num1*num2;
+let division= num1/num2;
+let modulo= num1%num2;
+console.log(`${suma} es la suma`)
+console.log(`${resta} es la resta`)
+console.log(`${mult} es la multiplicación`)
+console.log(`${division} es la división`)
+console.log(`${modulo} es el módulo`)
+
+/*
+console.log("Este es el número 1: "+String(num1));
+console.log(`${a} es el número entero a`);
+console.log(`${b} es el número entero b`);
+*/
---
+++ ejercicios1.js
@@ -0,0 +1,10 @@
+/*  
+
+Numeroetermina el mayor Numeroe tres números: 
+Numeroeclara tres variables a, b y c con valores numéricos y utiliza conNumeroicionales 
+para Numeroeterminar cuál Numeroe ellos es el mayor.
+
+*/
+let a= parseInt(prompt("Ingresa el número entero(a): "))
+let b= parseInt(prompt("Ingresa el número entero(b): "))
+let c= parseInt(prompt("Ingresa el número entero(c): "))
